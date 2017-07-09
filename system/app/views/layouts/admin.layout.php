022d8894a10e323298fa7140906d02f61fee642a
Mustafa Aydemir <mustafa@aydemir.im>
2017-07-09 15:51:34 +0300

    admin panel

--- system/app/views/layouts/admin.layout.php
+++ system/app/views/layouts/admin.layout.php
@@ -72,34 +72,6 @@ $types = cx::type();
 
         <?= layout::content() ?>
 
-        <!-- sidenav -->
-        <ul id="slide-out" class="side-nav">
-            <li><div class="user-view">
-                    <div class="background">
-                        <img src="images/office.jpg">
-                    </div>
-                    <a href="#!user"><img class="circle" src="images/yuna.jpg"></a>
-                    <a href="#!name"><span class="white-text name">John Doe</span></a>
-                    <a href="#!email"><span class="white-text email">jdandturk@gmail.com</span></a>
-                </div></li>
-            <li><a href="#!"><i class="material-icons">cloud</i>First Link With Icon</a></li>
-            <li><a href="#!">Second Link</a></li>
-            <li><div class="divider"></div></li>
-            <li><a class="subheader">Subheader</a></li>
-            <li><a class="waves-effect" href="#!">Third Link With Waves</a></li>
-        </ul>
-        <a href="#" data-activates="slide-out" class="button-collapse"><i class="material-icons">menu</i></a>
-        <script>
-            $('.button-collapse').sideNav({
-
-                menuWidth    : 300,
-                edge         : 'left',
-                closeOnClick : true,
-                draggable    : true
-            });
-        </script>
-        <!-- sidenav -->
-
         <a href="<?=URL?>" target="_blank" class="btn-floating cx-fixed btn-large waves-effect waves-light red modal-trigger"><i class="material-icons">home</i></a>
     </div>
 
