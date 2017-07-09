42169a0fee8db346d87ca5c6322e253184f7c954
Mustafa Aydemir <mustafa@aydemir.im>
2017-07-09 14:18:27 +0300

    admin login

--- system/app/views/sys/login.view.php
+++ system/app/views/sys/login.view.php
@@ -1,29 +1,38 @@
-<form id="admin_login_form" action="<?=SYS_URL?>/login" method="post">
-    <div class="section">
+<div class="center-box">
+    <div class="center-content">
 
-        <div class="row">
-            <div class="col l2 offset-l5">
-                <div class="card grey lighten-4">
-                    <div class="card-content">
-                        <div class="row">
-                            <div class="input-field col l12">
-                                <input name="user" type="text" class="validate" autocomplete="off">
-                                <label>Username or Email</label>
+        <form id="admin_login_form" action="<?=SYS_URL?>/login" method="post">
+            <div class="section">
+
+                <div class="row">
+                    <div class="col l2 offset-l5">
+                        <div class="card red white-text">
+                            <div class="card-content">
+                                <div class="row">
+                                    <div class="input-field col l12">
+                                        <input name="user" type="text" class="validate" autocomplete="off">
+                                        <label>Username or Email</label>
+                                    </div>
+                                    <div class="input-field col l12">
+                                        <input name="pass" type="password" class="validate" autocomplete="off">
+                                        <label>Password</label>
+                                    </div>
+
+                                </div>
                             </div>
-                            <div class="input-field col l12">
-                                <input name="pass" type="password" class="validate" autocomplete="off">
-                                <label>Password</label>
-                            </div>
-                            <div class="input-field col l12">
-                                <button class="btn green">Login</button>
+                            <div class="divider"></div>
+                            <div class="grey lighten-4">
+                                <button type="submit">Login</button>
                             </div>
                         </div>
                     </div>
                 </div>
+
             </div>
-        </div>
+        </form>
 
     </div>
-</form>
+</div>
 
-<script src="<?=SYS_ASSETS?>/admin/script.js"></script>
+<?= _css (SYS_ASSETS . '/admin/style.css') ?>
+<?= _js  (SYS_ASSETS . '/admin/script.js') ?>
