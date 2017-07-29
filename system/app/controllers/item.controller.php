e1d7a021e99d198c0ac38cdac52502aae35f3664
Mustafa Aydemir <mustafa@aydemir.im>
2017-07-29 15:37:13 +0300

    system/app/controllers/item.controller.php

--- system/app/controllers/item.controller.php
+++ system/app/controllers/item.controller.php
@@ -42,7 +42,6 @@ class controller_item extends controller
             $type_file = $data['type'];
             $view_file = $type_file ? $type_file : 'item/index';
 
-            _r( _data('item.url.parameters') );
             echo _render($view_file, ['data' => $data, 'parameters' => _data('item.url.parameters')], $render);
 
             // Views
